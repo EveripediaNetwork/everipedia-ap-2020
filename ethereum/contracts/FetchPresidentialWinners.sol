2ddca93b2fe1d5a29206811e8795eee81ddf0e3b
Dawson Botsford <dawsonbotsford@gmail.com>
2020-10-30 18:06:27 -0600

    ♦️  Add solidity files v0

---
+++ ethereum/contracts/FetchPresidentialWinners.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "TestnetConsumer.sol";
+
+contract FetchPresidentialWinners {
+    struct Winner {
+      string winner;
+      uint256 resultNow;
+      uint256 resultBlock;
+    }
+    
+    ATestnetConsumer internal winners;
+    
+    constructor() public {
+        winners = ATestnetConsumer(0x12B7B8Dea45AF31b6303E00C735332A8b6752856);
+    }
+    
+    
+   function selectWinner(string memory _state) public view returns (string memory winner, uint256 resultNow, uint256 resultBlock)
+  {
+    (string memory winner, uint256 resultNow, uint256 resultBlock) = winners.presidentialWinners(_state);
+    return (winner, resultNow, resultBlock);
+  }
+}
